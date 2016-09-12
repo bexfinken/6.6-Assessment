cc1dc8620d00cd84438dafcb205895375c168d50
bexfinken <bex.finken@gmail.com>
2016-09-12 10:09:25 -0500

    Methods

--- ballet.rb
+++ ballet.rb
@@ -1,16 +1,39 @@
-class Dancer
-  attr_reader: name
-  attr_acessor: age
+class Dancer  
+  attr_reader :name
+  attr_accessor :age
+  attr_accessor :card 
+
   def initialize(name, age)
     @name = name
     @age = age
+    @card = []
   end
 
-  def get_name(name)
+  def get_name
     @name
   end 
 
-  def get_age
+  def get_age(age)
       @age
   end
+
+  def pirouette
+    puts "*twirls*"
+  end 
+
+  def bow
+    puts "*bows*"
+  end
+
+  def queue_dance_with(partner)
+    @card << partner
+  end
+
+  def begin_next_dance
+      dance_partner = card.shift
+      p "Now dancing with #{dance_partner}"
+  end
+
 end
+
+p get_name
